a4635d627e41629745c74bfa67a06caa85a3803d
Vaidotas <vaidotas@gedzius.lt>
2022-03-13 14:07:31 +0700

    Ability to pan

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,12 +1,21 @@
-import p5, { Shader } from "p5";
+import p5, { Shader, Vector } from "p5";
 
-let shader: Shader;
-
-let zoom = 1.5;
-let offset = [-0.74364388703, 0.13182590421];
-let step = 1.5;
+const offsetStep = 0.005;
+const zoomStep = 1.5;
 
 function sketch(p: p5) {
+  let shader: Shader;
+  let zoom = 1.5;
+  let offset = [-0.7, 0];
+  let lastPosition = p.createVector(0, 0, 0);
+  let dragLocked = false;
+
+  let bx = 0.0;
+  let by = 0.0;
+
+  let xOffset = 0.0;
+  let yOffset = 0.0;
+
   p.preload = () => {
     shader = p.loadShader(
       "shaders/mandelbrot.vert.glsl",
@@ -20,7 +29,6 @@ function sketch(p: p5) {
   };
 
   p.draw = () => {
-    //const r = 1.5 * p.exp(-6.5 * (1 + p.sin(p.millis() / 2000)));
     shader.setUniform("r", zoom);
     shader.setUniform("p", offset);
 
@@ -30,12 +38,44 @@ function sketch(p: p5) {
 
   p.mouseWheel = (event: WheelEvent) => {
     if (event.deltaY > 0) {
-      zoom *= step;
+      zoom *= zoomStep;
     } else {
-      zoom /= step;
+      zoom /= zoomStep;
     }
 
     zoom = p.constrain(zoom, 0, 1.5);
+
+    return false;
+  };
+
+  p.mousePressed = (event: MouseEvent) => {
+    dragLocked = true;
+
+    xOffset = event.x - bx;
+    yOffset = event.y - by;
+  };
+
+  p.mouseReleased = (event: MouseEvent) => {
+    dragLocked = false;
+  };
+
+  p.mouseDragged = (event: DragEvent) => {
+    if (dragLocked) {
+      bx = event.x - xOffset;
+      by = event.y - yOffset;
+
+      const position = p.createVector(bx, by, 0);
+      const delta = lastPosition.sub(position);
+
+      offset = [
+        offset[0] + delta.x * offsetStep * zoom,
+        offset[1] - delta.y * offsetStep * zoom,
+      ];
+
+      lastPosition = position;
+    }
+
+    return false;
   };
 }
 
